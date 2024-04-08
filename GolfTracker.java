ca5632c8d3e28d80aaaedc3dd5687f36a43feed9
Corey Stamper <cstamper93@gmail.com>
2024-04-07 23:32:00 -0400

    Intro added

---
+++ GolfTracker.java
@@ -0,0 +1,21 @@
+public class GolfTracker {
+
+    /*
+    Flow:
+    > Intro
+    > Ask user to input one score at a time, followed by par for the course
+    > Prompt user to enter more scores or quit
+    > When they do quit, perform operations on input and return scoring average and handicap
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("<<<<<***** Golf Tracker *****>>>>>\n");
+        System.out.println("Welcome to Golf Tracker! Just enter your scores, followed by " +
+                "par for the course. \nYou will continue to be prompted to add more scores, " +
+                "or to quit. Your scoring average and handicap will be calculated when you " +
+                "finish. \nHappy scoring!");
+
+
+    }
+}
