8d055c0948ab080009078babb68114a230be01fe
Corey Stamper <cstamper93@gmail.com>
2024-04-10 00:12:48 -0400

    Added while loop to repeat series of questions

--- GolfTracker.java
+++ GolfTracker.java
@@ -1,3 +1,7 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GolfTracker {
 
     /*
@@ -10,12 +14,33 @@ public class GolfTracker {
 
     public static void main(String[] args) {
 
-        System.out.println("<<<<<***** Golf Tracker *****>>>>>\n");
+        System.out.println("\n<<<<<***** Golf Tracker *****>>>>>\n");
         System.out.println("Welcome to Golf Tracker! Just enter your scores, followed by " +
                 "par for the course. \nYou will continue to be prompted to add more scores, " +
                 "or to quit. Your scoring average and handicap will be calculated when you " +
                 "finish. \nHappy scoring!");
 
+        ArrayList<Integer> scores = new ArrayList<>();
+        ArrayList<Integer> parList = new ArrayList<>();
+
+        boolean run = true;
+        while(run) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter your total score: ");
+            int score = Integer.parseInt(scanner.nextLine());
+            scores.add(score);
+            System.out.println("What was par for the course?: ");
+            int par = Integer.parseInt(scanner.nextLine());
+            parList.add(par);
+            System.out.println("Would you like to enter another score? (Y/N): ");
+            String choice = scanner.nextLine();
+            if(choice.toUpperCase().equals("N")) {
+                run = false;
+            }
+        }
+
+        System.out.println(scores);
+        System.out.println(parList);
 
     }
 }
