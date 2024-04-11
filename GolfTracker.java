9982ed26228354d1242090ecdf220b3e7a108a23
Corey Stamper <cstamper93@gmail.com>
2024-04-11 14:04:42 -0400

    Added exception handling, calculations, and while loops for user experience

--- GolfTracker.java
+++ GolfTracker.java
@@ -12,6 +12,16 @@ public class GolfTracker {
     > When they do quit, perform operations on input and return scoring average and handicap
      */
 
+    /*
+    Competencies Demonstrated:
+    > Knowledge and application of variables, data types, and collections
+    > Handling exceptions
+    > Application of conditionals and loops
+    > Casting data types into other types
+    > How to concatenate strings
+    > User input
+     */
+
     public static void main(String[] args) {
 
         System.out.println("\n<<<<<***** Golf Tracker *****>>>>>\n");
@@ -21,26 +31,71 @@ public class GolfTracker {
                 "finish. \nHappy scoring!");
 
         ArrayList<Integer> scores = new ArrayList<>();
-        ArrayList<Integer> parList = new ArrayList<>();
+        ArrayList<Integer> scoreToParList = new ArrayList<>();
+
+        int score = 0;
 
         boolean run = true;
         while(run) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Please enter your total score: ");
-            int score = Integer.parseInt(scanner.nextLine());
-            scores.add(score);
-            System.out.println("What was par for the course?: ");
-            int par = Integer.parseInt(scanner.nextLine());
-            parList.add(par);
-            System.out.println("Would you like to enter another score? (Y/N): ");
-            String choice = scanner.nextLine();
-            if(choice.toUpperCase().equals("N")) {
-                run = false;
+            boolean completedScoreInput = false;
+            while (!completedScoreInput) {
+                try {
+                    System.out.println("Please enter your total score: ");
+                    score = Integer.parseInt(scanner.nextLine());
+                    scores.add(score);
+                    completedScoreInput = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a whole number.");
+                }
+            }
+            boolean completedParInput = false;
+            while(!completedParInput) {
+                try {
+                    System.out.println("What was par for the course?: ");
+                    int par = Integer.parseInt(scanner.nextLine());
+                    int scoreToPar = score - par;
+                    scoreToParList.add(scoreToPar);
+                    completedParInput = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a whole number.");
+                }
+            }
+            boolean continueAdding = true;
+            while (continueAdding) {
+                System.out.println("Would you like to enter another score? (Y/N): ");
+                String choice = scanner.nextLine();
+                if (choice.equalsIgnoreCase("N")) {
+                    continueAdding = false;
+                    run = false;
+                } else if (!choice.equalsIgnoreCase("Y")) {
+                    System.out.println("Invalid input. Please enter the letter Y for yes or N for no.");
+                } else {
+                    continueAdding = false;
+                }
             }
         }
 
-        System.out.println(scores);
-        System.out.println(parList);
+        int scoresSum = 0;
+        for (int i = 0; i < scores.size(); i++) {
+            scoresSum += scores.get(i);
+        }
+        int scoringAvg = scoresSum / scores.size();
+
+        int scoresToParSum = 0;
+        for (int i = 0; i < scoreToParList.size(); i++) {
+            scoresToParSum += scoreToParList.get(i);
+        }
+        int handicap = scoresToParSum / scoreToParList.size();
+
+        System.out.println("\n*************************");
+        System.out.println("Scoring Average: " + scoringAvg);
+        if (handicap >= 0) {
+            System.out.println("Handicap: " + handicap);
+        } else {
+            System.out.println("Handicap: +" + Math.abs(handicap));
+        }
+        System.out.println("*************************");
 
     }
 }
